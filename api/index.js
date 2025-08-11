f2da4f46d8cd3f060d2b3ec15c629fce9de6ec59
Dhia <sarrajdhia9999@gmail.com>
2025-08-11 09:33:12 +0100

    Center loader SVG

--- api/index.js
+++ api/index.js
@@ -11,12 +11,17 @@ export default async (req, res)=>{
                     <title>Calendar</title>
                     <style>
                         body{
-                            background-color: #000000;
                             margin: 0;
                             padding: 0;
                         }
+                        main{
+                            width: 1000px;
+                            height: 650px;
+                            display:flex;
+                            align-items: center;
+                            justify-content: center;
+                        }
                         #loader{
-                            margin: 250px 400px;
                             animation: loader-spin 0.8s steps(8) infinite;
                         }
                         @keyframes loader-spin{
@@ -30,19 +35,21 @@ export default async (req, res)=>{
                     </style>
                 </head>
                 <body>
-                    <svg id="loader" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" width="64px" height="64px" viewBox="0 0 128 128" xml:space="preserve">
-                        <g>
-                            <path d="M38.52 33.37L21.36 16.2A63.6 63.6 0 0 1 59.5.16v24.3a39.5 39.5 0 0 0-20.98 8.92z" fill="#979a51"/>
-                            <path d="M38.52 33.37L21.36 16.2A63.6 63.6 0 0 1 59.5.16v24.3a39.5 39.5 0 0 0-20.98 8.92z" fill="#e5e6d4" transform="rotate(45 64 64)"/>
-                            <path d="M38.52 33.37L21.36 16.2A63.6 63.6 0 0 1 59.5.16v24.3a39.5 39.5 0 0 0-20.98 8.92z" fill="#e5e6d4" transform="rotate(90 64 64)"/>
-                            <path d="M38.52 33.37L21.36 16.2A63.6 63.6 0 0 1 59.5.16v24.3a39.5 39.5 0 0 0-20.98 8.92z" fill="#e5e6d4" transform="rotate(135 64 64)"/>
-                            <path d="M38.52 33.37L21.36 16.2A63.6 63.6 0 0 1 59.5.16v24.3a39.5 39.5 0 0 0-20.98 8.92z" fill="#e5e6d4" transform="rotate(180 64 64)"/>
-                            <path d="M38.52 33.37L21.36 16.2A63.6 63.6 0 0 1 59.5.16v24.3a39.5 39.5 0 0 0-20.98 8.92z" fill="#e5e6d4" transform="rotate(225 64 64)"/>
-                            <path d="M38.52 33.37L21.36 16.2A63.6 63.6 0 0 1 59.5.16v24.3a39.5 39.5 0 0 0-20.98 8.92z" fill="#e5e6d4" transform="rotate(270 64 64)"/>
-                            <path d="M38.52 33.37L21.36 16.2A63.6 63.6 0 0 1 59.5.16v24.3a39.5 39.5 0 0 0-20.98 8.92z" fill="#e5e6d4" transform="rotate(315 64 64)"/>
-                        </g>
-                    </svg>
-                    <img id="calendar" alt="Contribution Calendar"/>
+                    <main>
+                        <svg id="loader" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" width="64px" height="64px" viewBox="0 0 128 128" xml:space="preserve">
+                            <g>
+                                <path d="M38.52 33.37L21.36 16.2A63.6 63.6 0 0 1 59.5.16v24.3a39.5 39.5 0 0 0-20.98 8.92z" fill="#979a51"/>
+                                <path d="M38.52 33.37L21.36 16.2A63.6 63.6 0 0 1 59.5.16v24.3a39.5 39.5 0 0 0-20.98 8.92z" fill="#e5e6d4" transform="rotate(45 64 64)"/>
+                                <path d="M38.52 33.37L21.36 16.2A63.6 63.6 0 0 1 59.5.16v24.3a39.5 39.5 0 0 0-20.98 8.92z" fill="#e5e6d4" transform="rotate(90 64 64)"/>
+                                <path d="M38.52 33.37L21.36 16.2A63.6 63.6 0 0 1 59.5.16v24.3a39.5 39.5 0 0 0-20.98 8.92z" fill="#e5e6d4" transform="rotate(135 64 64)"/>
+                                <path d="M38.52 33.37L21.36 16.2A63.6 63.6 0 0 1 59.5.16v24.3a39.5 39.5 0 0 0-20.98 8.92z" fill="#e5e6d4" transform="rotate(180 64 64)"/>
+                                <path d="M38.52 33.37L21.36 16.2A63.6 63.6 0 0 1 59.5.16v24.3a39.5 39.5 0 0 0-20.98 8.92z" fill="#e5e6d4" transform="rotate(225 64 64)"/>
+                                <path d="M38.52 33.37L21.36 16.2A63.6 63.6 0 0 1 59.5.16v24.3a39.5 39.5 0 0 0-20.98 8.92z" fill="#e5e6d4" transform="rotate(270 64 64)"/>
+                                <path d="M38.52 33.37L21.36 16.2A63.6 63.6 0 0 1 59.5.16v24.3a39.5 39.5 0 0 0-20.98 8.92z" fill="#e5e6d4" transform="rotate(315 64 64)"/>
+                            </g>
+                        </svg>
+                        <img id="calendar" alt="Contribution Calendar"/>
+                    </main>
                     <script>
                         const load = ()=>{
                             const img = document.getElementById("calendar");
